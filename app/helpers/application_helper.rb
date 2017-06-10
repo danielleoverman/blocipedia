c6574eefac547eb76f6d5b7642153e684b0b6713
danielleoverman <danielleoverman1@gmail.com>
2017-06-09 23:08:09 -0400

    Markdown changes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,18 @@ module ApplicationHelper
         content_tag :div, capture(&block), class: 'form-group'
     end
    end
+
+  def markdown(text)
+    options = {
+      filter_html:     true,
+      hard_wrap:       true,
+      space_after_headers: true,
+      fenced_code_blocks: true
+    }
+
+    renderer = Redcarpet::Render::HTML.new(options)
+    markdown = Redcarpet::Markdown.new(renderer, extensions)
+
+    markdown.render(text).html_safe
+  end   
 end
